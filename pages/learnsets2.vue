76a15d3fcf01246ad48c152e65055dc9bb8aea36
valentinWild <74301124+valentinWild@users.noreply.github.com>
2024-06-26 13:39:20 +0200

    tests

---
+++ pages/learnsets2.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>{{ response }}</div>
+</template>
+
+<script setup>
+
+import axios from 'axios';
+    const client = useSupabaseClient();
+    const user = useSupabaseUser();
+    console.log(user.value.id);
+
+    const response = await axios.get('http://localhost:3000/api/styleQuiz?limit=5');
+
+    console.log(response);
+
+</script>
+
+<style>
+
+
+</style>
