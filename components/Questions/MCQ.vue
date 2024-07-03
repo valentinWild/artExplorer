6529b771167102748d9c2a01434652a9c7f41f4c
ainee-jane <an-ni-ka@gmx.de>
2024-07-03 12:26:47 +0200

    Feedback for correct answer is displayed

--- components/Questions/MCQ.vue
+++ components/Questions/MCQ.vue
@@ -1,102 +1,104 @@
 <script setup>
+import { ref, watch, defineProps, defineEmits } from 'vue';
+
 const props = defineProps({
-    quizData: {
-        type: Object,
-        required: true,
-    },
-    questionAnswered: {
-        type: Boolean,
-        required: true,
-    },
-    itemResult: {
-        type: Object,
-        required: false,
-    }
+  quizData: {
+    type: Object,
+    required: true,
+  },
+  questionAnswered: {
+    type: Boolean,
+    required: true,
+  },
+  itemResult: {
+    type: Object,
+    required: false,
+  }
 });
 
 const emit = defineEmits(['submitItem']);
 
-const stem = ref();
+const stem = ref(props.quizData.stem || '');
 const options = ref([]);
 const selected = ref();
 const resultText = ref('');
 const resultClass = ref('');
+const correctAnswer = ref('');
 
 const getOptions = () => {
-    if (!props.quizData) {
-        return new Error('No Quiz data');
-    } 
-    options.value = props.quizData.answers_options.map(option => ({ value: option.id, label: option.value }));
+  if (!props.quizData || !props.quizData.answers_options) {
+    return new Error('No Quiz data');
+  }
+  options.value = props.quizData.answers_options.map(option => ({ value: option.id, label: option.value }));
 }
 
 getOptions();
 
 watch(() => props.itemResult, (newValue) => {
-    if (props.itemResult?.answered === true) {
-        console.log(props.itemResult);
-        generateResultText();
-    } else {
-        resultText.value = "";
-        resultClass.value = "";
-        selected.value = null;
-    }
+  if (props.itemResult?.answered === true) {
+    generateResultText();
+  } else {
+    resultText.value = "";
+    resultClass.value = "";
+    selected.value = null;
+  }
 });
 
 watch(() => props.quizData, (newValue) => {
-    if (props.quizData?.answers_options) {
-        getOptions();
-    } else {
-
-    }
+  if (props.quizData?.answers_options) {
+    getOptions();
+    stem.value = props.quizData.stem;
+  }
 });
 
 const generateResultText = () => {
-    const result = props.itemResult;
-    if (result.points > 0) {
-        resultText.value = "Correct!";
-        resultClass.value = "correct";
-    } else {
-        resultText.value = "Wrong!";
-        resultClass.value = "wrong";
-    }
+  const result = props.itemResult;
+  if (result.points > 0) {
+    resultText.value = "Correct!";
+    resultClass.value = "correct";
+  } else {
+    correctAnswer.value = result.content.correct_answers.map(answer => answer.value).join(', ');
+    resultText.value = `Wrong! The correct answer is: ${correctAnswer.value}`;
+    resultClass.value = "wrong";
+  }
 }
 </script>
 
 <template>
-    <div class="questions-container">
-        <img v-if="props.quizData.image_url" :src="props.quizData.image_url" alt="Quiz Image" class="quiz-image" />
-        <URadioGroup v-model="selected" :legend="quizData.stem" :options="options" />
-            <div class="button-container">
-                <UButton class="button-done"
-                    v-show="!questionAnswered"
-                    @click="$emit('submitItem', selected)"
-                >Done</UButton>
-            </div>
-        <div :class="['result', resultClass]">{{ resultText }}</div>
+  <div class="questions-container">
+    <img v-if="props.quizData.image_url" :src="props.quizData.image_url" alt="Quiz Image" class="quiz-image" />
+    <URadioGroup v-model="selected" :legend="stem" :options="options" />
+    <div class="button-container">
+      <UButton class="button-done"
+        v-show="!questionAnswered"
+        @click="$emit('submitItem', selected)"
+      >Done</UButton>
     </div>
+    <div :class="['result', resultClass]">{{ resultText }}</div>
+  </div>
 </template>
 
 <style scoped>
 .quiz-image {
-  max-height: 500px; 
-  margin-bottom: 20px; 
+  max-height: 500px;
+  margin-bottom: 20px;
   align-self: center;
   border-radius: 10px;
 }
 
 .button-done {
-  margin-top: 20px; 
+  margin-top: 20px;
 }
 
 .result {
-    color: #050505;
+  color: #050505;
 }
 
 .correct {
-    color: green;
+  color: green;
 }
 
 .wrong {
-    color: red;
+  color: red;
 }
 </style>
