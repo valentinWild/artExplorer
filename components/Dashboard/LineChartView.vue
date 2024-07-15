25983df43a8f9751fabde75fd1dcf50469b6b230
labipetra <117845863+labipetra@users.noreply.github.com>
2024-07-15 20:29:28 +0200

    Different color coding for all possible learnsets

--- components/Dashboard/LineChartView.vue
+++ components/Dashboard/LineChartView.vue
@@ -86,14 +86,18 @@ const colors = {
   Impressionism: 'rgba(255, 99, 132, 0.2)',
   Renaissance: 'rgba(54, 162, 235, 0.2)',
   Modernism: 'rgba(75, 192, 192, 0.2)',
-  // Add more colors as needed for other learning sets
+  Surrealism: 'rgba(153, 102, 255, 0.2)',
+'Pop Art': 'rgba(255, 159, 64, 1)',
+  '21st Century': 'rgba(255, 206, 86, 1)',
 };
 
 const borderColor = {
   Impressionism: 'rgba(255, 99, 132, 1)',
   Renaissance: 'rgba(54, 162, 235, 1)',
   Modernism: 'rgba(75, 192, 192, 1)',
-  // Add more border colors as needed for other learning sets
+  Surrealism: 'rgba(153, 102, 255, 1)',
+ 'Pop Art': 'rgba(255, 159, 64, 1)',
+  '21st Century': 'rgba(255, 206, 86, 1)',
 };
 
 const updateChart = () => {
